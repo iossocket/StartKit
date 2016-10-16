480d56e4c2cabdd99a8f456aabef3f14bc900aad
iossocket <avx302@gmail.com>
2016-10-16 21:28:50 +0800

    [ZhuXueliang] - add base api manager and protocol.

---
+++ StartKit/APIProtocol.swift
@@ -0,0 +1,22 @@
+//
+//  BaseAPIProtocol.swift
+//  StartKit
+//
+//  Created by XueliangZhu on 10/16/16.
+//  Copyright © 2016 ThoughtWorks. All rights reserved.
+//
+
+import Foundation
+
+enum RequestMethod {
+    case get
+    case post
+}
+
+enum RequestEncoding {
+    case json
+}
+
+protocol APIProtocol {
+    func request(_ url: String, method: RequestMethod, encoding: RequestEncoding, params: Dictionary<String, Any>, headers: Dictionary<String, Any>, success: @escaping (_ result: Any) -> Void, failure: @escaping (_ error: Error) -> Void)
+}
---
+++ StartKit/BaseAPIManager.swift
@@ -0,0 +1,40 @@
+//
+//  BaseAPIManager.swift
+//  StartKit
+//
+//  Created by XueliangZhu on 10/16/16.
+//  Copyright © 2016 ThoughtWorks. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class BaseAPIManager: APIProtocol {
+    
+    func request(_ url: String, method: RequestMethod, encoding: RequestEncoding, params: Dictionary<String, Any>, headers: Dictionary<String, Any>, success: @escaping (_ result: Any) -> Void, failure: @escaping (_ error: Error) -> Void) {
+        Alamofire.request(url, method: alamofireMethod(method), parameters: params, encoding: alamofireEncoding(encoding), headers: nil).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                success(value)
+            case .failure(let error):
+                failure(error)
+            }
+        }
+    }
+    
+    private func alamofireMethod(_ baseRequestMethod: RequestMethod) -> HTTPMethod {
+        switch baseRequestMethod {
+        case .get:
+            return HTTPMethod.get
+        case .post:
+            return HTTPMethod.post
+        }
+    }
+    
+    private func alamofireEncoding(_ encoding: RequestEncoding) -> ParameterEncoding {
+        switch encoding {
+        case .json:
+            return JSONEncoding.default
+        }
+    }
+}
