46f3990d7adcf24908c843484df77e988ff27bb9
iossocket <avx302@gmail.com>
2016-10-16 20:19:42 +0800

    [ZhuXueliang] - login page ui.

---
+++ StartKit/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  StartKit
+//
+//  Created by XueliangZhu on 9/24/16.
+//  Copyright © 2016 ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func loginAction(_ sender: AnyObject) {
+        
+    }
+}
