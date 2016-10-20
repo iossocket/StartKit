a1cccc805219b30e22457fe6305c3959a8750aed
iossocket <avx302@gmail.com>
2016-10-20 08:40:37 +0800

    [ZhuXueliang] - add save token feature.

---
+++ StartKit/KeychainSecretStore.swift
@@ -0,0 +1,28 @@
+//
+//  KeychainSecretStore.swift
+//  StartKit
+//
+//  Created by XueliangZhu on 10/20/16.
+//  Copyright © 2016 ThoughtWorks. All rights reserved.
+//
+
+import Foundation
+import KeychainAccess
+
+struct KeychainSecretStore: SecretStoreProtocol {
+    
+    let keychain: Keychain
+    let GITHUB_TOKEN = "github_token"
+    
+    init() {
+        keychain = Keychain()
+    }
+    
+    func saveToken(token: String) {
+        keychain[GITHUB_TOKEN] = token
+    }
+    
+    func getToken() -> String? {
+        return keychain[GITHUB_TOKEN]
+    }
+}
---
+++ StartKit/SecretStoreProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  SecretStoreProtocol.swift
+//  StartKit
+//
+//  Created by XueliangZhu on 10/20/16.
+//  Copyright © 2016 ThoughtWorks. All rights reserved.
+//
+
+import Foundation
+
+protocol SecretStoreProtocol {
+    func saveToken(token: String)
+    func getToken() -> String?
+}
