2e614c531dd29709b8b7b3b4f4ab36c8ad9592cc
iossocket <avx302@gmail.com>
2016-10-27 16:03:18 +0800

    [ZhuXueliang] - abstract base url for user api manager.

--- StartKit/UserAPIManager.swift
+++ StartKit/UserAPIManager.swift
@@ -21,6 +21,7 @@ enum UserResult<T> {
 
 class UserAPIManager {
     private let baseApiManager: APIProtocol
+    private let BASEURL = "https://api.github.com"
     
     init(baseApiManager: APIProtocol = BaseAPIManager()) {
         self.baseApiManager = baseApiManager
@@ -31,7 +32,7 @@ class UserAPIManager {
         let base64Credentials = credentialData.base64EncodedString()
         let headers = ["Authorization": "Basic \(base64Credentials)"]
         let params: Dictionary<String, Any> = ["scopes": ["repo", "user"], "note": "GitHubDemo"]
-        baseApiManager.request("https://api.github.com/authorizations", method: .post, encoding: .json, params: params, headers: headers, success: { result in
+        baseApiManager.request(BASEURL + "/authorizations", method: .post, encoding: .json, params: params, headers: headers, success: { result in
             print(result)
             guard let dict = result as? Dictionary<String, Any>, let token = dict["token"] as? String else {
                 handler(.failure(error: .loginFailed))
@@ -45,7 +46,7 @@ class UserAPIManager {
     
     func profile(token: String, handler: @escaping (UserResult<User>) -> Void) {
         let headers = ["Authorization": "token \(token)"]
-        baseApiManager.request("https://api.github.com/user", method: .get, encoding: .json, params: nil, headers: headers, success: { result in
+        baseApiManager.request(BASEURL + "/user", method: .get, encoding: .json, params: nil, headers: headers, success: { result in
             guard let user = Mapper<User>().map(JSONObject: result) else {
                 handler(.failure(error: .profileFailed))
                 return
