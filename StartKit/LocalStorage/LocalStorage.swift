42725ad996cf4763665274db1636bc57ec43a75f
Xin Guo <emagrorrimxguo@gmail.com>
2018-05-13 02:41:00 +0800

    [GuoXin] Introduce coredata into project

---
+++ StartKit/LocalStorage/LocalStorage.swift
@@ -0,0 +1,13 @@
+//
+//  LocalStorage.swift
+//  StartKit
+//
+//  Created by Xin Guo  on 2018/5/13.
+//  Copyright © 2018 ThoughtWorks. All rights reserved.
+//
+
+import Foundation
+
+protocol LocalStorage {
+  func save<T>(object: T)
+}
